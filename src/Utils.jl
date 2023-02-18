0d207ddc5a11c6b2f25d176cba84a656c168bf75
Michael Bennett <michael.d.a.bennett@outlook.com>
2023-02-18 16:12:40 -0500

    add functions to utils module

---
+++ src/Utils.jl
@@ -0,0 +1,22 @@
+module Utils
+
+"""
+    make_k_grid(min, max, N)
+
+Make a grid for the capital stock.
+
+# Arguments
+
+- `min::Real`: Minimum value of the grid
+- `max::Real`: Maximum value of the grid
+- `N::Int`: Number of grid points
+
+# Returns
+
+- `k_grid::Vector{Float64}`: Grid points for the capital stock
+"""
+function make_k_grid(min::Real, max::Real, N::Integer)
+    return collect(Float64, range(min, max, N))
+end
+
+end # module
