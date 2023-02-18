7b5c4757e35364d47d6287680dbe6bd989f713b9
Michael Bennett <michael.d.a.bennett@outlook.com>
2023-02-18 17:00:25 -0500

    update exports

--- src/BellmanSolver.jl
+++ src/BellmanSolver.jl
@@ -1,13 +1,15 @@
 module BellmanSolver
 
-export make_k_grid
-export tauchen, tauchen_unit_root, make_deterministic_chain
-export do_VFI
-export do_EGM
-
 include("Utils.jl")
 include("Markov.jl")
 include("VFI.jl")
 include("EGM.jl")
+
+using .Utils, .Markov, .VFI, .EGM
+
+export make_k_grid
+export tauchen, tauchen_unit_root, make_deterministic_chain
+export do_VFI
+export do_EGM
 
 end # module
