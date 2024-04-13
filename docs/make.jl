77e967b98eed364372ef1157c6aa03c872864b8d
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-04-12 22:56:52 -0400

    try deploying

--- docs/make.jl
+++ docs/make.jl
@@ -4,6 +4,12 @@ makedocs(
     sitename="BellmanSolver.jl",
     format = Documenter.HTML(; prettyurls = true),
     pages=[
-        "index.md"
+        "index.md",
+        "utils.md",
+        "markov.md",
+        "VFI.md",
+        "egm.md"
     ]
 )
+
+deploydocs("github.com/michaelbennett99/BellmanSolver.jl")
